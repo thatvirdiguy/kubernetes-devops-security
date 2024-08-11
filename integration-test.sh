3ee1a3ee967a794dad385403b26e087c5f796f83
Aman <amanCJ24@gmail.com>
2024-08-11 16:44:03 +0530

    fixed typo

--- integration-test.sh
+++ integration-test.sh
@@ -7,7 +7,7 @@ PORT=$(kubectl -n default get svc ${serviceName} -o json | jq .spec.ports[].node
 echo $PORT
 echo $applicationURL:$PORT/$applicationURI
 
-if [[ | -z "$PORT" ]]; then
+if [[ ! -z "$PORT" ]]; then
 
   response=$(curl -s $applicationURL:$PORT$applicationURI)
   http_code=$(curl -s -o /dev/null -w "%{http_code}" $applicationURL:$PORT$applicationURI)
