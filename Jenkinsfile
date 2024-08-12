37026014bc2a347f1586abff672c95f5c9f81b04
Aman <amanCJ24@gmail.com>
2024-08-12 23:11:25 +0530

    adding content rich slack notification: test

--- Jenkinsfile
+++ Jenkinsfile
@@ -1,5 +1,43 @@
 @Library('slack') _
 
+// *** Code for fetching Failed Stage Name *** //
+
+import io.jenkins.blueocean.rest.impl.pipeline.PipelineNodeGraphVisitor
+import io.jenkins.blueocean.rest.impl.pipeline.FlowNodeWrapper
+import org.jenkinsci.plugins.workflow.support.steps.build.RunWrapper
+import org.jenkinsci.plugins.workflow.actions.Error action
+
+// Get information about all stages, including the failure cases
+// Returns a list of maps: [[ id, failedStageName, result, errors ]]
+@NonCPS
+List<Map> getStageResults( RunWrapper build ) {
+
+  // Get all pipeline nodes that represent stages
+  def visitor = new PipelineNodeGraphVisitor( build.rawBuild )
+  def stages = visitor.pipelineNodes.findAll{ it.type == FlowNodeWrapper.NodeType.STAGE }
+
+  return stages.collect{ stage ->
+    // Get all the errors from the stage
+    def errorActions = stage.getPipelineActions( ErrorAction )
+    def errors = errorActions?.collect{ it.error }.unique()
+
+    return [
+      id: stage_id,
+      failedStageName: stage.displayName,
+      result: "${stage.status.result}",
+      errors: errors
+    ]
+  }  
+}
+
+// Get information of all failed stages
+@NonCPS
+List<Map> getFailedStages( RunWrapper build ) {
+  return getStageResults( build ).findAll{ it.result == 'FAILURE' }
+}
+
+// *** Code for fetching Failed Stage Name *** //
+
 pipeline {
 
   agent any
@@ -12,8 +50,11 @@ pipeline {
     applicationURL = "http://devsecops-demo.southindia.cloudapp.azure.com"
     applicationURI = "/increment/99"
   }
-  
+
   stages {
+
+    /*
+
       stage('Build Artifact - Maven') {
         steps {
           sh "mvn clean package -DskipTests=true"
@@ -175,7 +216,15 @@ pipeline {
           }
         }
       }
+
+    */
+
+      stage('Testing Slack: Success') {
+        exit 0;
+      }
+
   }
+
 
   post {
     always  {
@@ -184,8 +233,30 @@ pipeline {
       pitmutation mutationStatsFile: '**/target/pit-reports/**/mutations.xml'
       dependencyCheckPublisher pattern: 'target/dependency-check-report.xml'
       publishHTML([allowMissing: false, alwaysLinkToLastBuild: true, keepAll: true, reportDir: 'owasp-zap-report', reportFiles: 'zap_report.html', reportName: 'OWASP ZAP HTML Report', reportTitles: 'OWASP ZAP HTML Report', useWrapperFileDirectly: true])
-      sendNotification currentBuild.result
+      // use sendNotification.groovy from shared library and provide current build result as parameter
+      // sendNotification currentBuild.result
     }
+
+    success {
+      script {
+        // use slackNotifier.groovy from shared library and provide current build result as parameter
+        env.failedStage = "none"
+        env.emoji = ":white_check_mark: :tada:"
+        slackNotifier currentBuild.result
+      }
+    }
+
+    failure {
+      script {
+        // use slackNotifier.groovy from shared library and provide current build result as parameter
+        def failedStages = getFailedStages( currentBuild )
+        env.failedStage = failedStages.failedStageName
+        env.emoji = ":red_circle: :sos:"
+        slackNotifier currentBuild.result
+      }
+    }
+
+
   }
 
 }
---
+++ vars/slackNotifier.groovy
@@ -0,0 +1,112 @@
+def call(String buildStatus = 'STARTED') {
+  buildStatus = buildStatus ?: 'SUCCESS'
+  
+  def color
+
+  if (buildStatus == 'SUCCESS') {
+    color = '#47ec05'
+    emoji = ':smile:'
+  }
+  else if (buildStatus == 'UNSTABLE') {
+    color = '#d5ee0d'
+    emoji = ':confused:'
+  }
+  else {
+    color = '#ec2805'
+    emoji = ':frowning:'
+  }
+  
+  attachments = [
+    [
+        "color": color,
+        "blocks": [
+            [
+                "type": "header",
+                "text": [
+                    "type": "plain_text",
+                    "text": "Kubernetes Deployment - ${deploymentName Pipeline ${env.emoji}",
+                    "emoji": true
+                ]
+            ],
+            [
+                "type": "section",
+                "fields": [
+                    [
+                        "type": "mrkdwn",
+                        "text": "*Job Name:*\n${env.JOB_NAME}"
+                    ],
+                    [
+                        "type": "mrkdwn",
+                        "text": "*Build Number:*\n${env.BUILD_NUMBER}"
+                    ]
+                ],
+                "accessory": [
+                    "type": "image",
+                    "image_url": "https://raw.githubusercontent.com/sidd-harth/numeric/main/images/jenkins-slack.png",
+                    "alt_text": "Slack Icon"
+                ]
+            ],
+            [
+                "type": "section",
+                "text": [
+                    "type": "mrkdwn",
+                    "text": "Failed Stage Name: * `${env.failedStage}`"
+                ],
+                "accessory": [
+                    "type": "button",
+                    "text": [
+                        "type": "plain_text",
+                        "text": "Jenkins Build URL",
+                        "emoji": true
+                    ],
+                    "value": "click_me_123",
+                    "url": "${env.BUILD_URL}",
+                    "action_id": "button-action"
+                ]
+            ],
+            [
+                "type": "divider"
+            ],
+            [
+                "type": "section",
+                "fields": [
+                    [
+                        "type": "mrkdwn",
+                        "text": "*Git Commit:*\n${GIT_COMMIT}"
+                    ],
+                    [
+                        "type": "mrkdwn",
+                        "text": "*GIT Previous Success Commit:*\n$GIT_PREVIOUS_SUCCESSFUL_COMMIT}"
+                    ]
+                ],
+                "accessory": [
+                    "type": "image",
+                    "image_url": "https://raw.githubusercontent.com/sidd-harth/numeric/main/images/github-slack.png",
+                    "alt_text": "GitHub Icon"
+                ]
+            ],
+            [
+                "type": "section",
+                "text": [
+                    "type": "mrkdwn",
+                    "text": "*Git Branch: * `${GIT_BRANCH}`"
+                ],
+                "accessory": [
+                    "type": "button",
+                    "text": [
+                        "type": "plain_text",
+                        "text": "GitHub Repo URL",
+                        "emoji": true
+                    ],
+                    "value": "click_me_123",
+                    "url": "${env.GIT_URL}",
+                    "action_id": "button-action"
+                ]
+            ]
+        ]
+    ]
+]
+
+slackSend(iconEmoji: emoji, attachments: attachments)
+  
+}
